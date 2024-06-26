8d7d9f831e74e0387083ec84a0fefe5c529d3187
YunhoNam <nam100400@naver.com>
2024-06-26 22:23:15 +0900

    Update #3 - EventService to turn Exception into GeneralException
    이벤트 서비스가 데이터에 접근할 때 에러가 나는 경우, 일반 케이스 에러로 변환하여 내보내는 로직 추가

--- management_system/src/main/java/com/yunho/management_system/service/EventService.java
+++ management_system/src/main/java/com/yunho/management_system/service/EventService.java
@@ -1,7 +1,9 @@
 package com.yunho.management_system.service;
 
+import com.yunho.management_system.constant.ErrorCode;
 import com.yunho.management_system.constant.EventStatus;
 import com.yunho.management_system.dto.EventDTO;
+import com.yunho.management_system.exception.GeneralException;
 import com.yunho.management_system.repository.EventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,28 +25,53 @@ public class EventService {
             LocalDateTime eventStartDatetime,
             LocalDateTime eventEndDatetime
     ) {
-        return eventRepository.findEvents(
-                placeId,
-                eventName,
-                eventStatus,
-                eventStartDatetime,
-                eventEndDatetime
-        );
+        try {
+            return eventRepository.findEvents(
+                    placeId,
+                    eventName,
+                    eventStatus,
+                    eventStartDatetime,
+                    eventEndDatetime
+            );
+        }
+        catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
+        }
     }
 
     public Optional<EventDTO> getEvent(Long eventId) {
-        return eventRepository.findEvent(eventId);
+        try {
+            return eventRepository.findEvent(eventId);
+        }
+        catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
+        }
     }
 
     public boolean createEvent(EventDTO eventDTO) {
-        return eventRepository.insertEvent(eventDTO);
+        try {
+            return eventRepository.insertEvent(eventDTO);
+        }
+        catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
+        }
     }
 
     public boolean modifyEvent(Long eventId, EventDTO dto) {
-        return eventRepository.updateEvent(eventId, dto);
+        try {
+            return eventRepository.updateEvent(eventId, dto);
+        }
+        catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
+        }
     }
 
     public boolean removeEvent(Long eventId) {
-        return eventRepository.deleteEvent(eventId);
+        try {
+            return eventRepository.deleteEvent(eventId);
+        }
+        catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
+        }
     }
 }
