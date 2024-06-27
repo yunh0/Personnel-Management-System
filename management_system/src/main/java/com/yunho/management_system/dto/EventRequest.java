0ab61819a51a3a059a7395863014b329de620a56
YunhoNam <nam100400@naver.com>
2024-06-27 21:38:29 +0900

    Update #10 - place information in dto
    placeId -> place를 내어주면서 dto 스펙도 변경함

--- management_system/src/main/java/com/yunho/management_system/dto/EventRequest.java
+++ management_system/src/main/java/com/yunho/management_system/dto/EventRequest.java
@@ -44,7 +44,7 @@ public record EventRequest(
     public EventDto toDTO() {
         return EventDto.of(
                 null,
-                this.placeId(),
+                null,
                 this.eventName(),
                 this.eventStatus(),
                 this.eventStartDatetime(),
