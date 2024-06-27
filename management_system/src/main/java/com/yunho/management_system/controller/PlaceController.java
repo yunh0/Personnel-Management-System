fa1e0dc869dbee5bea129090e73b61a8bce7e599
YunhoNam <nam100400@naver.com>
2024-06-27 15:35:58 +0900

    Implement #10 - PlaceService & PlaceController

--- management_system/src/main/java/com/yunho/management_system/controller/PlaceController.java
+++ management_system/src/main/java/com/yunho/management_system/controller/PlaceController.java
@@ -1,22 +1,51 @@
 package com.yunho.management_system.controller;
 
+import com.querydsl.core.types.Predicate;
+
+import com.yunho.management_system.domain.Place;
+import com.yunho.management_system.dto.PlaceResponse;
+import com.yunho.management_system.exception.GeneralException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RequiredArgsConstructor
 @RequestMapping("/places")
 @Controller
 public class PlaceController {
 
+    private final PlaceService placeService;
+
     @GetMapping
-    public String places() {
-        return "place/index";
+    public ModelAndView places(@QuerydslPredicate(root = Place.class) Predicate predicate) {
+        Map<String, Object> map = new HashMap<>();
+        List<PlaceResponse> places = placeService.getPlaces(predicate)
+                .stream()
+                .map(PlaceResponse::from)
+                .toList();
+        map.put("places", places);
+
+        return new ModelAndView("place/index", map);
     }
 
     @GetMapping("/{placeId}")
-    public String placeDetail(@PathVariable Long placeId) {
-        return "place/detail";
+    public ModelAndView placeDetail(@PathVariable Long placeId) {
+        Map<String, Object> map = new HashMap<>();
+        PlaceResponse place = placeService.getPlace(placeId)
+                .map(PlaceResponse::from)
+                .orElseThrow(() -> new GeneralException(ErrorCode.NOT_FOUND));
+
+        map.put("place", place);
+
+        return new ModelAndView("place/detail", map);
     }
 
 }
---
+++ management_system/src/main/java/com/yunho/management_system/service/PlaceService.java
@@ -0,0 +1,81 @@
+package com.yunho.management_system.service;
+
+import com.querydsl.core.types.Predicate;
+
+import com.yunho.management_system.constant.ErrorCode;
+import com.yunho.management_system.dto.PlaceDto;
+import com.yunho.management_system.exception.GeneralException;
+import com.yunho.management_system.repository.PlaceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.StreamSupport;
+
+@RequiredArgsConstructor
+@Service
+public class PlaceService {
+
+    private final PlaceRepository placeRepository;
+
+    public List<PlaceDto> getPlaces(Predicate predicate) {
+        try {
+            return StreamSupport.stream(placeRepository.findAll(predicate).spliterator(), false)
+                    .map(PlaceDto::of)
+                    .toList();
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
+        }
+    }
+
+    public Optional<PlaceDto> getPlace(Long placeId) {
+        try {
+            return placeRepository.findById(placeId).map(PlaceDto::of);
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
+        }
+    }
+
+    public boolean createPlace(PlaceDto placeDto) {
+        try {
+            if (placeDto == null) {
+                return false;
+            }
+
+            placeRepository.save(placeDto.toEntity());
+            return true;
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
+        }
+    }
+
+    public boolean modifyPlace(Long placeId, PlaceDto dto) {
+        try {
+            if (placeId == null || dto == null) {
+                return false;
+            }
+
+            placeRepository.findById(placeId)
+                    .ifPresent(place -> placeRepository.save(dto.updateEntity(place)));
+
+            return true;
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
+        }
+    }
+
+    public boolean removePlace(Long placeId) {
+        try {
+            if (placeId == null) {
+                return false;
+            }
+
+            placeRepository.deleteById(placeId);
+            return true;
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
+        }
+    }
+
+}
